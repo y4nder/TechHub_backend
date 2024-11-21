58bdf5a578de42ebda9363f57968dfc4314be9ae
Yander <lorenzolubguban@gmail.com>
2024-11-21 20:00:37 +0800

    added new migrations

---
+++ infrastructure/Migrations/20241121074557_AddedClubViewPropertyToEntityClub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedClubViewPropertyToEntityClub : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClubViews",
+                table: "Club",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClubViews",
+                table: "Club");
+        }
+    }
+}
